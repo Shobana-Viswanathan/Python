42142934c557f12e8b525724e46c77611f670f63
Shobana-Viswanathan <shobanav112@gmail.com>
2026-05-26 14:10:55 +0530

    Improve item entry loop and total cost calculation
    Refactor item entry loop for clarity and consistency.

--- Handson1_Sequence/Hard5.py
+++ Handson1_Sequence/Hard5.py
@@ -1,9 +1,15 @@
-total=0
-while True :
-   price=int(input("Enter the price of the item: "))
-   quantity=int(input("Enter the quantity of the item: w"))
-   option=input("Do you want to enter another item?(yes/no)")
-   total=total+(price*quantity)
-   if(option=='no'):
-    break
-print("Total Price: ",total)
+total = 0
+
+while True:
+
+    price = int(input("Enter the price of the item: "))
+    quantity = int(input("Enter the quantity of the item: "))
+
+    total = total + (price * quantity)
+
+    choice = input("Do you want to enter another item?(yes/no) ")
+
+    if choice.lower() == "no":
+        break
+
+print("Total Cost:", total)
