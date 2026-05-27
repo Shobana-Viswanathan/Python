49e2b0f28ca829aa3a853e2677cd69c6eac29648
Shobana <shobanav112@gmail.com>
2026-05-27 22:51:26 +0530

    Added handson

--- Handson4_String/Easy4.py
+++ Handson4_String/Easy4.py
@@ -1,2 +1,7 @@
-str1=input()
-for i in range str1 :
+str1 = input("Enter string: ")
+
+words = str1.split()
+
+for i in words:
+    if i.isalnum() and not i.isalpha():
+        print(i)
--- Handson4_String/Medium2.py
+++ Handson4_String/Medium2.py
@@ -1,3 +1,3 @@
-str1=input("Enter string")
-print("First character of String: ",str1[0])
-print("Last character of String: ",str1[len(str1) - 1])
+String = input("Enter the string:")
+print("First character of string:",String[0])
+print("Last character of string:",String[len(String)-1])
